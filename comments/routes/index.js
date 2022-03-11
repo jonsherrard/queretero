8e014924a05fac28d84646c0ff8deebd1e409755
Jonathon Sherrard <jon@devular.com>
2022-03-11 18:17:43 +0000

    Removes static comments and adds server rendering of comments from the database
    A "readCommments" method has been added to the comment service.
    
    When the index page is loaded, we request the latest 10 comments and pass them into the index template.

--- comments/routes/index.js
+++ comments/routes/index.js
@@ -1,9 +1,23 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const commentService = require("../services/comment");
+const TimeAgo = require("javascript-time-ago");
+const en = require("javascript-time-ago/locale/en.json");
+
+TimeAgo.addDefaultLocale(en);
+const timeAgo = new TimeAgo("en-US");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", async function (req, res, next) {
+  const comments = await commentService.readComments({ limit: 10 });
+  const timeAgoComments = comments.map((comment) => {
+    return {
+      ...comment,
+      timeAgo: timeAgo.format(comment.createdAt),
+    };
+  });
+  console.log({ timeAgoComments });
+  res.render("index", { title: "Express", comments: timeAgoComments });
 });
 
 module.exports = router;
