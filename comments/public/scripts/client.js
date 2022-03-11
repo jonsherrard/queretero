cc5c175a815eb6f6e72d2ffeed50f3ba577a1af6
Jonathon Sherrard <jon@devular.com>
2022-03-11 21:47:51 +0000

    Adds React App for upvotes using htm instead of pre-compiling JSX
    - Each upvote block is a React app using HTM
    - HTM is used in place of JSX and a build step

--- comments/public/scripts/client.js
+++ comments/public/scripts/client.js
@@ -24,29 +24,61 @@ commentForm.addEventListener("submit", function (event) {
   }
 });
 
-const comments = document.querySelectorAll("[data-upvote-comment-id]");
+const { createElement, useState } = React;
+const render = ReactDOM.render;
+const html = htm.bind(createElement);
+
+function ClickCounter({ initialUpvoteCount, commentId }) {
+  const [count, setCount] = useState(initialUpvoteCount);
+
+  return html`
+    <a
+      onClick=${() => {
+        setCount(count + 1);
+        var request = new XMLHttpRequest();
+        var url = "/api/upvotes/";
+        request.open("POST", url, true);
+        request.setRequestHeader("Content-Type", "application/json");
+        request.onreadystatechange = function () {
+          if (request.readyState === 4 && request.status === 201) {
+            var jsonData = JSON.parse(request.response);
+          }
+          if (request.readyState === 4 && request.status === 500) {
+            var jsonData = JSON.parse(request.response);
+            setCount(count - 1);
+          }
+        };
+        var data = JSON.stringify({
+          commentId: commentId,
+        });
+        request.send(data);
+      }}
+      className="mr-6 cursor-pointer hover:text-purple-500"
+    >
+      <div
+        className="relative inline-block w-3 mr-1 overflow-hidden hover:text-purple-600 bottom-px"
+      >
+        <div
+          className="w-2 h-2 origin-bottom-left transform rotate-45 bg-gray-600 hover:bg-purple-500"
+        ></div>
+      </div>
+      Upvote (${count})
+    </a>
+  `;
+}
+
+const comments = document.querySelectorAll(".app-upvote");
 comments.forEach(function (el) {
-  el.addEventListener("click", function (event) {
-    var request = new XMLHttpRequest();
-    var url = "/api/upvotes/";
-    request.open("POST", url, true);
-    request.setRequestHeader("Content-Type", "application/json");
-    request.onreadystatechange = function () {
-      if (request.readyState === 4 && request.status === 201) {
-        var jsonData = JSON.parse(request.response);
-        location.reload();
-      }
-      if (request.readyState === 4 && request.status === 500) {
-        var jsonData = JSON.parse(request.response);
-        alert(jsonData.message);
-      }
-    };
-    var commentId = this.dataset.upvoteCommentId;
-    var data = JSON.stringify({
-      commentId: commentId,
-    });
-    request.send(data);
-  });
+  console.log(el);
+  const initialUpvoteCount = parseInt(el.dataset.initialUpvoteCount);
+  const upvoteCommentId = parseInt(el.dataset.upvoteCommentId);
+  render(
+    html`<${ClickCounter}
+      initialUpvoteCount="${initialUpvoteCount}"
+      commentId="${upvoteCommentId}"
+    />`,
+    el
+  );
 });
 
 const replyButtons = document.querySelectorAll(".reply-button");
