6d0c72f5be532b8439d597ca6029636cd7b392fa
Jonathon Sherrard <jon@devular.com>
2022-03-12 00:46:58 +0000

    Replaces static name and image with random placeholders

--- comments/app.js
+++ comments/app.js
@@ -4,6 +4,8 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const hbs = require("hbs");
+var FakerHandlebarsHelper = require("handlebars-faker");
+hbs.registerHelper("faker", FakerHandlebarsHelper);
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
